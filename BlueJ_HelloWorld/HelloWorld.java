cc5bd95b9b803b62ae07e4d85c47c8fa5b1942ff
william2705 <william2705@cmis.ac.th>
2018-08-09 14:30:07 +0700

    Finished HelloWord

---
+++ BlueJ_HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+    {
+    public static void main ( String args[] )
+        {
+        System.out.println( "Hello everone out there! ");
+        System.out.println( "Yaet hopefully i'll pass this class with a 4 or higher! ");
+        } // end method main
+        
+        
+    } // end class HelloWorld
