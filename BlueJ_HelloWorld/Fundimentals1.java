bf0e45ed2b3dbe423b8b4647d057c1fadb6ee9cc
William Cai <william2705@cmis.ac.th>
2018-08-14 09:33:33 +0700

    started on fundimentals datatypes

---
+++ BlueJ_HelloWorld/Fundimentals1.java
@@ -0,0 +1,21 @@
+
+public class Fundimentals1
+{
+    public static void main ( String args[] ){
+        double time = 9.23;
+        int period = 2;
+        boolean coffee = true;
+        System.out.println("Time: " + time );
+        System.out.println(" Period: " + period );
+        System.out.println(" Do I have coffee: " + coffee);
+        return ;
+    }
+    
+    public static int gamingHours(){
+        int hours = 0;
+        hours = hours + 8;
+        System.out.println("My average gaming hours during the summer vacation is: " + hours);
+        return hours;
+    }
+    
+}
