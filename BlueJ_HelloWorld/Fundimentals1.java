84205168f08a8ef30ccf7f118b3eed5abb53ed00
William Cai <william2705@cmis.ac.th>
2018-08-15 11:57:47 +0700

    got the primitive things and string i just need the descriptions and the print of the strings to finish

--- BlueJ_HelloWorld/Fundimentals1.java
+++ BlueJ_HelloWorld/Fundimentals1.java
@@ -18,4 +18,31 @@ public class Fundimentals1
         return hours;
     }
     
+    public static void dataTypes(){
+        boolean open = true;
+        byte amountofplayers = 5;
+        short amountofhostiles = 24;
+        int amountofbullets = 31*50;
+        long amountofpixle = 9598592312358L;
+        float amountofspeed = 4.5F;
+        double defultmousesensitivity = 8.5D;
+        char siteA = 'A';
+        char siteB = 'B';
+        
+        String bool = "Boolean is 1 bit of information such as 0 and 1 or true and false";
+        String byt = "Byte is an 8-bit thing that you can use and is super small so it saves space";
+        String shot = "Short is 16-bit thing that you can use to save memory as a byte data type";
+        String intager = "Int is 32-bit and is used as a defult datatype unless there is a problem concerning the memory";
+        String lon = "Long is a 64-bit thing that is composed of a ton of int or a wider range of int. Has to add a L at the end of the number to be considered as a long, not an int";
+        String flote = "Float is a 32-bit non-precise data type that can include decimal but should not be used as something specific such as currency. Used to save up memory.";
+        String doubal = "Double is a 64-bit float that is used as a defult decimal choice but also shall not be used as something specific such as currency.";
+        String cher = "Char is a 16-bit thing that doesn't include numbers but it includes single letters using single quotation marks.";
+        
+        String boolex = "Some real life use of boolean is the decision of path between yes or no, true or false, 1 or 0.";
+        String bytex = "Some real life use of byte is for such as in a video game, it can be easy to store simple informations such as the amount of players where not a large amount of numbers are needed or the amount of guns there can be or even the amount of bullets each guns can carry";
+        String shotex = "Some real life use of short is to save up the memory that each byte information takes such as, the amount of players each gigantic server can contain, the total amount of bullets shooting out in a game of lets say, 2 thousand players.";
+        
+        
+    }
+    
 }
