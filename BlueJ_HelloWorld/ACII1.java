8b6f45acf14c58c844b8c5633526a64420345e27
William Cai <william2705@cmis.ac.th>
2018-08-21 19:11:22 +0700

    ASCII1 Work

---
+++ BlueJ_HelloWorld/ACII1.java
@@ -0,0 +1,34 @@
+
+public class ACII1
+{
+public static void main (String args[]){
+    System.out.println("            __.---.___");
+    System.out.println("        ___/__\\_O_/___\\___");
+    System.out.println("       /___ __________ ___\\ ");
+    System.out.println("       |===|\\________/|===| ");
+    System.out.println("_______|===|__________|===|______");
+}
+
+
+public static void game (String args[]){
+    int loop = 0;
+    for (loop = 0; loop >= 0; loop++){
+        if (loop == 5){
+         loop = -1;
+            
+            
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+}
+
