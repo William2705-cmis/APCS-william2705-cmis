b0358c265737da98cc34311e910b5368fb867eb1
William Cai <william2705@cmis.ac.th>
2018-09-05 09:55:57 +0700

    did a lot of checkerboard things

--- BlueJ_HelloWorld/checkerboard.java
+++ BlueJ_HelloWorld/checkerboard.java
@@ -3,25 +3,68 @@
 public class checkerboard
 {
     public static void checkerboard(int h, int w){
-        int width = w;
-     for(int luptime = h; h>0; h--)
-     {
-            System.out.print("|");
+            System.out.print("+");
+            int width = w;
+            
             for (int looptime = w; w > 0; w--)
-                {
-                    System.out.print(".");
-                    if (w == 1){
-                        System.out.print("| \n");
-                    }
+            {
+                System.out.print("-");
+                if (w == 1){
+                    System.out.print("+\n");
+                }
+            }
+            w = width;
+            //^^^^top boarder ^^^^^
+            for(int luptime = h; h>0; h--)
+         {
+                System.out.print("|");
+            if (h % 2 != 0 ){
+                System.out.print("#");
+                for (int looptime = w; w > 0; w--)
+                    {
+                        if (w == 1){
+                                System.out.print("|\n");
+                            }
+                            
+                            if (w % 2 != 0){
+                                System.out.print(" ");
+                            }
+                            else if (w % 2 == 0){
+                                System.out.print("#");
+                            }
+                            
                     
                 }
-        w = width;
-        
-        
-        
-        
-        
-            }       
+                w = width;
+            }
+            
+            //^^^^ odd line^^^^
+            else if ( h % 2 == 0 ){
+                for (int looptime = w; w > 0; w--)
+                    {
+                        
+                            System.out.print("#");
+                            if (w == 1){
+                                System.out.print("|\n");
+                            }
+                    
+                }
+                
+                w = width; 
+            }  
+            //^^^^ even line ^^^^
+        }
+            //^^^mid part^^^
+            w = width;
+            System.out.print("+");
+            for (int looptime = w; w > 0; w--)
+            {
+                System.out.print("-");
+                if (w == 1){
+                    System.out.print("+");
+                }
+            }
+            //^^^Bottom part^^^     
     }
     
     
