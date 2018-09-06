f28affae655fbac308f19376564f22448e1102a9
William Cai <william2705@cmis.ac.th>
2018-09-06 11:57:14 +0700

    checkerboard is the end

--- BlueJ_HelloWorld/checkerboardserious.java
+++ BlueJ_HelloWorld/checkerboardserious.java
@@ -1,7 +1,9 @@
 
-public class checkerboard
+public class checkerboardserious
 {
     public static void checkerboard(int h, int w){
+        
+        
         System.out.print("+");
         for (int looptimewidth = w; looptimewidth > 0; looptimewidth--)
             {
@@ -10,21 +12,12 @@ public class checkerboard
                     System.out.print("+\n");
                 }
     }
+            
         for(int looptimeheight = h; looptimeheight>0; looptimeheight--)
          {
             System.out.print("|");
-            if ( looptimeheight % 2 != 0){
-                for (int looptimewidth = w; looptimewidth > 1; looptimewidth--){
-                            if (looptimewidth % 2 != 0){
-                                System.out.print(" ");
-                            }
-                            else if (looptimewidth % 2 == 0 || looptimewidth != 0){
-                                System.out.print("#");
-                            }
-                }
-            }  
-             else if (looptimeheight % 2 == 0 ){
-                for (int looptimewidth = w; looptimewidth > 1; looptimewidth--){
+             if (looptimeheight % 2 == 0 ){
+                for (int looptimewidth = w; looptimewidth > 0; looptimewidth--){
                             if (looptimewidth % 2 == 0){
                                 System.out.print(" ");
                             }
@@ -33,9 +26,19 @@ public class checkerboard
                             }   
                  }
                }
-            
+            else if ( looptimeheight % 2 != 0 ){
+                for (int looptimewidth = w; looptimewidth > 0; looptimewidth--){
+                            if (looptimewidth % 2 != 0){
+                                System.out.print(" ");
+                            }
+                            else if (looptimewidth % 2 == 0){
+                                System.out.print("#");
+                            }
+                }
+            }  
             System.out.print("|\n");
         }
+        
             System.out.print("+");
             for (int looptimewidth = w; looptimewidth > 0; looptimewidth--)
                 {
