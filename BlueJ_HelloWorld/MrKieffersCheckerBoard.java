ad8ecc2d7cde06be06d1768d7036e6d9fc843a0c
William Cai <william2705@cmis.ac.th>
2018-09-10 14:20:13 +0700

    Mr kieffer helped us with the checkerboard

---
+++ BlueJ_HelloWorld/MrKieffersCheckerBoard.java
@@ -0,0 +1,39 @@
+public class MrKieffersCheckerBoard
+{
+    public static void main(String args[])
+    {
+        System.out.println(cb(5,5));
+    }
+    public static String cb(int h, int w){
+        String out = "";
+        String top = "+";
+        for(int x = 0; x < w; x++){
+            top += "-";
+        }
+         top += "+\n";
+         int nHashes = (h * w) / 2;
+         int rLocation = (int)(Math.random() * nHashes);
+         int hashCount = 0;
+        for(int y = 0; y < h; y++){
+            out += "|";
+            for(int x = 0; x < w; x++){
+                boolean isHash = ((x+y)%2) == 0;
+                if(isHash){
+                    if(rLocation == hashCount){
+                        out += (int)(Math.random() * 10);
+                    } else
+                    {
+                        out += "#";
+                    }
+                    hashCount++;
+                } 
+                else{
+                    out += " ";
+                }
+            }
+            out += "|\n";
+        }
+        out = top + out + top;
+        return out;
+    }
+}
