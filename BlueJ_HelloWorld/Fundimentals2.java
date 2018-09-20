334639ea7cfe8240b0f5e5d0efa4bc7f96e598eb
William Cai <william2705@cmis.ac.th>
2018-09-20 14:36:49 +0700

    I did sum stuff

---
+++ BlueJ_HelloWorld/Fundimentals2.java
@@ -0,0 +1,22 @@
+
+
+public class Fundimentals2
+{
+    public static void main(){
+        
+     String numbers[] = new String[]{"|","+","#","@","*","&","/","\\"," ", ":", "w"};
+     int w = 0;
+     int h = 10;
+     int z = 5;
+     int p = (int) Math.random() * 10;
+        for(int n = w; n <= h; n++){
+            
+            System.out.print(numbers[p]);
+            p++;
+            if (n == h){
+                
+            }
+    }
+    }
+}
+
