d2c4ff6f7bdf6faf1ae6de94ef941851d7580c60
william cai <william2705@cmis.ac.th>
2018-10-01 11:51:17 +0700

    Did 3 questions

--- BlueJ_HelloWorld/Fundimentals2.java
+++ BlueJ_HelloWorld/Fundimentals2.java
@@ -3,18 +3,38 @@
 public class Fundimentals2
 {
     public static void main(){
+        //#1
         int[] intagers = new int[]{0, 1, 2, 3, 4};
         double[] doubles = new double[]{0.2, 1.5, 2.3, 3.6, 4.1};
-        boolean[] arr3 = new boolean[]{true,false,true,false,true};
+        boolean[] booleans = new boolean[]{true,false,true,false,true};
         String[] strings = new String[]{"Hi", "Jeff", "I like to eat cheese", "这个系统能打中文吗?", "If it can, then it is 666"};
         
         System.out.println(intagers[0]);
         System.out.println(doubles[2]);
         System.out.println(strings[4]);
         
-        printArray(intagers,false);
+        printArray(intagers,true);
+        System.out.print("\n");
+        printArray(doubles,false);
+        System.out.print("\n");
+        printArray(booleans,true);
+        System.out.print("\n");
+        printArray(strings,false);
+        System.out.print("\n");
+        
+        System.out.println(lastItem(intagers));
+        System.out.println(lastItem(doubles));
+        System.out.println(lastItem(booleans));
+        System.out.println(lastItem(strings));
+        
+        System.out.println(middleItem(intagers));
+        System.out.println(middleItem(doubles));
+        System.out.println(middleItem(booleans));
+        System.out.println(middleItem(strings));
     }
     
+    
+    //#2
     public static void printArray(int array[], boolean skip){
         int shkip = 0;
         if(skip == true){
@@ -29,5 +49,94 @@ public class Fundimentals2
             }
         }
     }
-
+        public static void printArray(double array[], boolean skip){
+        int shkip = 0;
+        if(skip == true){
+            while(shkip<array.length){
+                System.out.print(array[shkip] + ", ");
+                shkip+=2;
+            }
+        }else{
+            while(shkip<array.length){
+                System.out.print(array[shkip] + ", ");
+                shkip++;
+            }
+        }
+    }
+    public static void printArray(boolean array[], boolean skip){
+        int shkip = 0;
+        if(skip == true){
+            while(shkip<array.length){
+                System.out.print(array[shkip] + ", ");
+                shkip+=2;
+            }
+        }else{
+            while(shkip<array.length){
+                System.out.print(array[shkip] + ", ");
+                shkip++;
+            }
+        }
+    }
+    public static void printArray(String array[], boolean skip){
+        int shkip = 0;
+        if(skip == true){
+            while(shkip<array.length){
+                System.out.print(array[shkip] + ", ");
+                shkip+=2;
+            }
+        }else{
+            while(shkip<array.length){
+                System.out.print(array[shkip] + ", ");
+                shkip++;
+            }
+        }
+    }
+    
+    
+    
+    //#3
+    public static int lastItem(int array[]){
+        int result = 0;
+        result = array[array.length - 1];
+        return result;
+    }
+    public static double lastItem(double array[]){
+        double result = 0;
+        result = array[array.length - 1];
+        return result;
+    }
+    public static boolean lastItem(boolean array[]){
+        boolean result = true;
+        result = array[array.length - 1];
+        return result;
+    }
+    public static String lastItem(String array[]){
+        String result = "";
+        result = array[array.length - 1];
+        return result;
+    }
+    
+    
+    
+    //#4
+    public static int middleItem(int array[]){
+        int result = 0;
+        result = array[array.length / 2];
+        return result;
+    }
+    public static double middleItem(double array[]){
+        double result = 0;
+        result = array[array.length / 2];
+        return result;
+    }
+    public static boolean middleItem(boolean array[]){
+        boolean result = true;
+        result = array[array.length / 2];
+        return result;
+    }
+    public static String middleItem(String array[]){
+        String result = "";
+        result = array[array.length / 2];
+        return result;
+    }
 }
