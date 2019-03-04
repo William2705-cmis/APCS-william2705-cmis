1e2421d141a2b886e366fe47b0dde61d215187b1
William Cai <william2705@cmis.ac.th>
2019-03-04 14:34:11 +0700

    card

---
+++ Cards/CardApp.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class CardApp here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CardApp
+{
+    public static void main (String[] args){
+        Card[] deck = new Card[52];
+        int d = 0;
+        
+        for(int rank = 0; rank < 13; rank++){
+            
+        }
+        
+        
+        
+    }
+    
+}
