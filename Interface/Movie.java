083915dd738bff0487b4f50ad95a188227d7c0be
William Cai <william2705@cmis.ac.th>
2019-05-09 09:55:57 +0700

    Interface

---
+++ Interface/Movie.java
@@ -0,0 +1,10 @@
+public class Movie
+{
+    String[] MovieList = new String[10];
+    public String welcome(){
+        return "Welcome";
+    }
+    public void main(){
+        System.out.println("This is a movie list");
+    }
+}
---
+++ Interface/MovieCharacteristic.java
@@ -0,0 +1,7 @@
+
+public interface MovieCharacteristic
+{
+    public String getName();
+    
+    public void output();
+}
